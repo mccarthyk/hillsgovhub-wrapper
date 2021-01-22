d33f0d2a6f1bc422ab0c24f9c97a0df56a9e8f4d
Kevin McCarthy <mccarthyk@hcflgov.net>
2021-01-22 11:59:26 -0500

    prep for CDN

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,11 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
-function LibPublicPathPlugin() {}
-
-LibPublicPathPlugin.prototype.apply = function(compiler) {
-  compiler.hooks.beforeRun.tap('LibPublicPathPlugin', (compiler) => {
-    compiler.options.output.publicPath = '/hcfl/Customization/HCFL/vue-wrapper/'
-  })
-}
-
 module.exports = {
-  outputDir: '../gitlab/Customization/HCFL/vue-wrapper',
-  productionSourceMap: false,
   chainWebpack: (config) => {
     config.resolve.alias.set('vue$', 'vue/dist/vue.esm.js')
   },
   configureWebpack: {
-    plugins: [new VuetifyLoaderPlugin(), new LibPublicPathPlugin()],
+    plugins: [new VuetifyLoaderPlugin()],
     output: {
       libraryExport: 'default',
     },
